ebc2f67e4468bb166677f143be8b768351b8c87b
dat-iro <138441382+dat-iro@users.noreply.github.com>
2023-09-24 20:14:22 +0200

    solution

--- exercise.js
+++ exercise.js
@@ -1,5 +1,45 @@
 class Person {
-  // ...
+  firstName;
+  lastName;
+  age;
+
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  set firstName(newFirstName) {
+    this.firstName = newFirstName;
+  }
+
+  get firstName() {
+    return this.firstName;
+  }
+
+  set lastName(newLastName) {
+    this.lastName = newLastName;
+  }
+
+  get lastName() {
+    return this.lastName;
+  }
+
+  set age(newAge) {
+    this.age = newAge;
+  }
+
+  get age() {
+    return this.age;
+  }
+
+  set fullName(newFullName) {
+    this.fullName = newFullName;
+  }
+
+  get fullName() {
+    return this.firstName + " " + this.lastName;
+  }
 }
 
 const person = new Person('Mario', 'Rossi', 25);
